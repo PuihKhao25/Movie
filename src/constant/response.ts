2dd745b718ab55a7ed9c457199bc62c453893238
PuihKhao25 <pkhao1999@gmail.com>
2023-01-05 12:25:03 +0700

    check

--- src/constant/response.ts
+++ src/constant/response.ts
@@ -1,17 +1,18 @@
 import { Response } from "express";
 import { ResError } from ".";
 
-export const SystemError = (res: Response, error: Error) =>{
-    console.log(error);
+export const SystemError = (res: Response, error: Error) => {
+  console.log(error);
   res.status(500).json({
     code: "SYS500",
     message: "System Error",
-  })};
+  });
+};
 
-export const ResponseFailed = (res: Response, message: any) =>{
-    console.log(message.message)
-    res.status(400).json(message);
-}
+export const ResponseFailed = (res: Response, message: any) => {
+  console.log(message.message);
+  res.status(400).json(message);
+};
 
 export const ResponseSuccess = (res: Response, content?: any) =>
   res.status(200).json({
